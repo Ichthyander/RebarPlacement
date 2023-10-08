80fb7964722b0106f728f4529559d3a3d6b73074
di_ignatov@mail.ru <di_ignatov@mail.ru>
2023-10-08 21:04:31 +0300

    Done

---
+++ Models/ElementSelectionFilters/RebarSelectionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI.Selection;
+
+namespace RebarPlacement.Models.ElementSelectionUtils
+{
+    public class RebarSelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element element)
+        {
+            return element is Rebar;
+        }
+
+        public bool AllowReference(Reference refer, XYZ point)
+        {
+            return false;
+        }
+    }
+}
---
+++ Models/ElementSelectionFilters/WallSelectionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace RebarPlacement.Models.ElementSelectionUtils
+{
+    public class WallSelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element element)
+        {
+            return element is Wall;
+        }
+
+        public bool AllowReference(Reference refer, XYZ point)
+        {
+            return false;
+        }
+    }
+}
